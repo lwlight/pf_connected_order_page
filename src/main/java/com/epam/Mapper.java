c87d4fa18adc2751cb5fa60a1005712a8ee84d85
Artsiom Kniha <Artsiom_Kniha@epam.com>
2018-09-10 22:51:46 +0300

    fix mapper id param

--- src/main/java/com/epam/Mapper.java
+++ src/main/java/com/epam/Mapper.java
@@ -5,9 +5,12 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 
 public class Mapper {
+    private static final String SFID_COLUMN = "SFID";
+    private static final String NAME_COLUMN = "Name";
+    
     static final RowMapper<Dish> DISH_ROW_MAPPER = (ResultSet resultSet, int rowNum) -> {
-        String id = resultSet.getString("Id");
-        String name = resultSet.getString("Name");
+        String id = resultSet.getString(SFID_COLUMN);
+        String name = resultSet.getString(NAME_COLUMN);
         Dish dish = new Dish(id, name);
 
         return dish;
